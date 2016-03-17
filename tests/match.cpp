d38a2c7296340a5da3c35a17303a3c2e3351a202
Vincent Lascaux <vincelas@microsoft.com>
2016-03-17 15:57:02 -0700

    Add match UT

--- tests/match.cpp
+++ tests/match.cpp
@@ -9,14 +9,20 @@ namespace goldfish
 
 	TEST_CASE(test_match)
 	{
-		auto x = first_match(
+		auto first = first_match(
 			[](B&&) { return 1; },
-			[](A&&) { return 2; }
+			[](A&&) { return 2; },
+			[](C&&) { return 3; }
 		);
 
-		test(x(A{}) == 2);
-		test(x(B{}) == 1);
-		test(x(C{}) == 2);
+		auto best = best_match(
+			[](B&&) { return 1; },
+			[](A&&) { return 2; },
+			[](C&&) { return 3; });
+
+		test(first(A{}) == 2); test(best(A{}) == 2);
+		test(first(B{}) == 1); test(best(B{}) == 1);
+		test(first(C{}) == 2); test(best(C{}) == 3);
 	}
 }
 
