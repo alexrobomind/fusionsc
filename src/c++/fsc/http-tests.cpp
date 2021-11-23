2c3a8c8783f8ad294a17290da51f8a9b53ce254b
Alexander Knieps <alexanderrobom@web.de>
2021-11-23 01:08:48 +0100

    Fixed error (by not referencing temporary header table)

--- src/c++/fsc/http-tests.cpp
+++ src/c++/fsc/http-tests.cpp
@@ -7,10 +7,7 @@
 
 namespace fsc {
 
-TEST_CASE("http") {
-	kj::_::Debug::setLogLevel(kj::LogSeverity::INFO);
-	//EnableFullStackTrace fullTrace;
-	
+TEST_CASE("http") {	
 	using kj::HttpMethod;
 	
 	Library l = newLibrary();
@@ -30,29 +27,24 @@ TEST_CASE("http") {
 		HTTP_TEST_DATA
 	);
 	
-	auto& active = srv.listen();
+	auto active = srv.listen();
 	
 	if(active.poll(ws))
 		// Uh oh, something failed during port binding. Print error.
 		active.wait(ws);
 	
 	unsigned int port = srv.getPort().wait(ws);
-	KJ_LOG(WARNING, port);
 	
 	auto remoteAddr = network.parseAddress("127.0.0.1", port).wait(ws);
-	KJ_LOG(WARNING, *remoteAddr);
 	
 	auto client = kj::newHttpClient(
 		th -> ioContext().provider -> getTimer(),
-		kj::HttpHeaderTable(),
+		hTbl,
 		*remoteAddr
 	);
-	//auto client = kj::newHttpClient(srv);
 	
 	auto response = client->request(HttpMethod::GET, "/", headers).response.wait(ws);
-	KJ_LOG(WARNING, "Got response");
 	auto body = response.body->readAllText().wait(ws);
-	KJ_LOG(WARNING, "Got body");
 	
 	REQUIRE(body == "Hello world");
 }
--- src/c++/fsc/http.cpp
+++ src/c++/fsc/http.cpp
@@ -11,29 +11,19 @@ SimpleHttpServer::SimpleHttpServer(Promise<Own<kj::NetworkAddress>> address, Lib
 	_port(nullptr),
 	_listen(nullptr)
 {
-	KJ_LOG(WARNING, "Making port info");
 	auto portInfo = kj::newPromiseAndFulfiller<unsigned int>();
-	//auto listenInfo = kj::newPromiseAndFulfiller<void>();
-	
-	KJ_LOG(WARNING, "Forking port info");
 	_port = portInfo.promise.fork();
-	//_listen = listenInfo.promise.fork();
 	
-	KJ_LOG(WARNING, "Starting server from address");
-	_server = address.then([this, portInfo = mv(portInfo)/*, listenInfo = mv(listenInfo)*/, lt = lt->addRef()](Own<kj::NetworkAddress> address) mutable {
-		KJ_LOG(WARNING, "Starting listener");
+	_server = address.then([this, portInfo = mv(portInfo), lt = lt->addRef()](Own<kj::NetworkAddress> address) mutable {
 		auto listener = address->listen();
 		portInfo.fulfiller->fulfill(listener->getPort());
 		
-		KJ_LOG(WARNING, "Making server...");
 		auto result = std::make_shared<kj::HttpServer>(
 			lt -> ioContext().provider -> getTimer(),
 			*_headerTable,
 			*this
 		);
 		
-		//result->listenHttp(*listener).then([listenInfo = mv(listenInfo)]() mutable { listenInfo.fulfiller->fulfill(); });
-		//KJ_LOG(WARNING, "Listening...");
 		_listen = result->listenHttp(*listener).attach(mv(listener)).fork();
 		
 		return result;
@@ -55,33 +45,22 @@ Promise<void> SimpleHttpServer::request(
 	kj::AsyncInputStream& requestBody,
 	Response& response
 ) {
-	using kj::HttpMethod;
-	KJ_LOG(WARNING, "Received request");
-	KJ_LOG(WARNING, url);
-	
+	using kj::HttpMethod;	
 	KJ_REQUIRE(method == HttpMethod::GET, "Can only process method GET");
 	
-	KJ_LOG(WARNING, "Iterating data");
 	for(auto e : _data -> getEntries()) {
-		KJ_LOG(WARNING, "Checking entry");
-		KJ_LOG(WARNING, e);
 		if(e.getLoc() != url)
 			continue;
 		
-		KJ_LOG(WARNING, "Found entry");
 		
 		KJ_REQUIRE(e.isText());
 		auto d = e.getText();
 		
-		KJ_LOG(WARNING, "Sending");
-		auto ostream = response.send(200, "OK", kj::HttpHeaders(*_headerTable)/*, e.getText().size()*/);
-		KJ_LOG(WARNING, "Writing");
+		auto ostream = response.send(200, "OK", kj::HttpHeaders(*_headerTable), e.getText().size());
 		auto result = ostream -> write(d.begin(), d.size());
-		KJ_LOG(WARNING, "Returning");
 		return result.attach(mv(ostream)).then([](){KJ_LOG(WARNING, "Write finished");});
 	}
 	
-	//return kj::NEVER_DONE;
 	return response.sendError(404, "Not found", kj::HttpHeaders(*_headerTable));	
 }
 
