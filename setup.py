29be7ddbfd3d35947bc438c8f80dee351f81b3af
Alexander Knieps <s6alknie@uni-bonn.de>
2022-07-22 16:01:02 +0200

    Incorrect download command

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
 	cmake_install_target = "fsc-install-skbuild",
 	entry_points = {
 		'console_scripts': [
-			'w7x-download = fsc.w7x.scripts:download'
+			'w7x-download = fsc.devices.w7x.scripts:download'
 		]
 	}
 )
