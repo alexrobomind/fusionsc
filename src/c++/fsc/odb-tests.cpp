e16cf4adbeb724c5fc071a1371fe43b1f3e7dfd4
Alexander Knieps <alexanderrobom@web.de>
2022-12-19 23:55:32 +0100

    Codecov increase

--- src/c++/fsc/odb-tests.cpp
+++ src/c++/fsc/odb-tests.cpp
@@ -22,21 +22,38 @@ TEST_CASE("ODB blobstore") {
 	builder.write(data1);
 	
 	Blob blob = builder.finish();
-	
+	REQUIRE_THROWS(builder.finish());
+		
 	auto data2 = kj::heapArray<byte>(1024);
 	{
 		auto reader = blob.open();
 		REQUIRE(reader.read(data2));
 		REQUIRE(reader.remainingOut() == 0);
+		REQUIRE(data1 == data2);
+	}
+	
+	{
+		auto reader = blob.open();
+		REQUIRE_FALSE(reader.read(data2.slice(0, 512)));
+		REQUIRE(reader.read(data2.slice(512, 1024)));
+		REQUIRE(reader.remainingOut() == 0);
+		REQUIRE(data1 == data2);
 	}
 		
-	REQUIRE(data1 == data2);
 	
 	KJ_IF_MAYBE(pResult, store -> find(blob.hash())) {
 		REQUIRE(true);
 	} else {
 		REQUIRE(false);
 	}
+	
+	auto builder2 = store -> create(128);
+	builder2.write(data1.slice(0, 213));
+	builder2.write(data1.slice(213, 1024));
+	
+	// Check that hashes are cached
+	Blob blob2 = builder2.finish();
+	REQUIRE(blob2.id == blob.id);
 	
 	REQUIRE(blob.refcount() == 0);
 	blob.incRef();
