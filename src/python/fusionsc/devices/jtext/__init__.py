24683a6ec9acf53eb037b28094eb7757d543b0d1
Alexander Knieps <s6alknie@uni-bonn.de>
2023-05-10 15:25:53 +0200

    Updates to device modules

--- src/python/fusionsc/devices/jtext/__init__.py
+++ src/python/fusionsc/devices/jtext/__init__.py
@@ -8,38 +8,38 @@ def defaultGeometryGrid() -> service.CartesianGrid.Reader:
 
 def firstWall():
 	result = geometry.Geometry()
-	result.geometry.initJtext().firstWall = None
+	result.data.initJtext().firstWall = None
 	return result
 
 def hfsLimiter():
 	result = geometry.Geometry()
-	result.geometry.initJtext().hfsLimiter = None
+	result.data.initJtext().hfsLimiter = None
 	return result
 
 def target():
 	result = geometry.Geometry()
-	result.geometry.initJtext().target = None
+	result.data.initJtext().target = None
 	return result
 
 def lfsLimiter(pos):
 	result = geometry.Geometry()
-	result.geometry.initJtext().lfsLimiter = pos
+	result.data.initJtext().lfsLimiter = pos
 	return result
 
 def topLimiter(pos):
 	result = geometry.Geometry()
-	result.geometry.initJtext().topLimiter = pos
+	result.data.initJtext().topLimiter = pos
 	return result
 
 def bottomLimiter(pos):
 	result = geometry.Geometry()
-	result.geometry.initJtext().bottomLimiter = pos
+	result.data.initJtext().bottomLimiter = pos
 	return result
 
 def singleIslandCoil(i, current):
 	result = magnetics.MagneticConfig()
 	
-	filField = result.field.initFilamentField()
+	filField = result.data.initFilamentField()
 	
 	filField.biotSavartSettings.width = 0.01
 	filField.biotSavartSettings.stepSize = 0.01
@@ -52,8 +52,8 @@ def singleIslandCoil(i, current):
 def islandCoils(currents):
 	result = magnetics.MagneticConfig()
 	
-	result.field.sum = [
-		singleIslandCoil(i, currents[i-1]).field
+	result.data.sum = [
+		singleIslandCoil(i, currents[i-1]).data
 		for i in [1, 2, 3, 4, 5, 6]
 	]
 	
