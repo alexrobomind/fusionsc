770377a4d7b4f33a6cf929e38722b57a7ae92a47
Alexander Knieps <s6alknie@uni-bonn.de>
2023-05-17 15:20:12 +0200

    Removed w7x-download script

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
 	cmake_install_target = "fsc-install-skbuild",
 	entry_points = {
 		'console_scripts': [
-			'w7x-download = fusionsc.devices.w7x.scripts:download'
+			#'w7x-download = fusionsc.devices.w7x.scripts:download'
 		]
 	},
 	
