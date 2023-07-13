de07df6b084b9bda8178e93f7a7684c24d09d0a8
Alexander Knieps <s6alknie@uni-bonn.de>
2023-07-13 15:17:59 +0200

    Documentation update

--- build-docs.bat
+++ build-docs.bat
@@ -1,21 +1,23 @@
 :: Builds the documentation and pushes it to the main repository
 :: Builds from 'docs' to 'docs' (because of github pages limitations)
 
+:: Checkout
 git checkout pages
 git reset --hard main
 
+:: Build documentations into "docs" dir in src tree
 rmdir /s /q docs
-
 cd build
 cmake --build . --target copy-docs --config Release
 cd ..
 
+:: Disable the Jekyll preprocessor
+echo "No Jekyll" > docs\.nojekyll
+
+:: Commit & force push
 git add -A docs
 git commit -m "Documentation update"
 git push -f origin pages
 
-:: Go back to main and delete documentation
+:: Go back to main
 git checkout main
-
-rmdir /s /q docs
-
