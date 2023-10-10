a24bcbf86ed4dd3cd72e2b7400a19fecf923916c
Alexander Knieps <s6alknie@uni-bonn.de>
2023-10-10 15:23:50 +0200

    Moved botan amalgamation out of source tree

--- src/cmake/CompileBotan.cmake
+++ src/cmake/CompileBotan.cmake
@@ -11,29 +11,35 @@ else()
 	set(BOTAN_XARGS "")
 endif()
 
-# Note: FetchContent explicitly creates lower-case named variables
+#file(COPY ${Botan_SOURCE_DIR}/ DESTINATION ${Botan_BINARY_DIR})
+
+set(AMALGAMATION_DIR "${Botan_BINARY_DIR}/amalgamation/$<CONFIG>")
+
 add_custom_command(
-	OUTPUT "${Botan_BINARY_DIR}/botan_all.cpp"
+	OUTPUT "${AMALGAMATION_DIR}/botan_all.cpp"
+	
+	COMMAND ${CMAKE_COMMAND} -E copy_directory ${Botan_SOURCE_DIR} ${AMALGAMATION_DIR}
+	
 	COMMAND ${Python3_EXECUTABLE}
-	"${Botan_SOURCE_DIR}/configure.py"
+	"${AMALGAMATION_DIR}/configure.py"
 	"--minimized-build"
 	"--enable-modules=auto_rng,blake2,sha2_32,sha2_64,system_rng"
 	"--amalgamation"
 	"--disable-shared"
 	"--cc-bin" "${CMAKE_CXX_COMPILER}"
-	"--with-build-dir" "${Botan_BINARY_DIR}"
+	"--with-build-dir" "${AMALGAMATION_DIR}"
 	${BOTAN_XARGS}
 	
-	COMMAND ${CMAKE_COMMAND} -E copy "${Botan_SOURCE_DIR}/botan_all.h" "${Botan_BINARY_DIR}/botan_all.h"
-	COMMAND ${CMAKE_COMMAND} -E copy "${Botan_SOURCE_DIR}/botan_all.cpp" "${Botan_BINARY_DIR}/botan_all.cpp"
+	#COMMAND ${CMAKE_COMMAND} -E copy "${Botan_SOURCE_DIR}/botan_all.h" "${Botan_BINARY_DIR}/botan_all.h"
+	#COMMAND ${CMAKE_COMMAND} -E copy "${Botan_SOURCE_DIR}/botan_all.cpp" "${Botan_BINARY_DIR}/botan_all.cpp"
 	
-	WORKING_DIRECTORY ${Botan_SOURCE_DIR}
+	WORKING_DIRECTORY ${AMALGAMATION_DIR}
 )
 
-add_library(botan_selfbuilt "${Botan_BINARY_DIR}/botan_all.cpp")
-target_include_directories(botan_selfbuilt INTERFACE "$<BUILD_INTERFACE:${Botan_BINARY_DIR}/build/include>")
+add_library(botan_selfbuilt "${AMALGAMATION_DIR}/botan_all.cpp")
+target_include_directories(botan_selfbuilt INTERFACE "$<BUILD_INTERFACE:${AMALGAMATION_DIR}/build/include>")
 target_include_directories(botan_selfbuilt INTERFACE "$<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>")
-set_property(TARGET botan_selfbuilt PROPERTY BUILT_HEADERS "${Botan_BINARY_DIR}/build/include")
+set_property(TARGET botan_selfbuilt PROPERTY BUILT_HEADERS "${AMALGAMATION_DIR}/build/include")
 
 
 add_library(Botan::botan ALIAS botan_selfbuilt)
