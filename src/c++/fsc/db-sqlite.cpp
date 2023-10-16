7f9652d548390966f1b87ede4cfadc6ba9b177ad
Alexander Knieps <s6alknie@uni-bonn.de>
2023-10-16 18:20:36 +0200

    New DB interface

---
+++ src/c++/fsc/db-sqlite.cpp
@@ -0,0 +1,238 @@
+#include "db.h"
+#include "sqlite3.h"
+
+#incldue <kj/refcount.h>
+
+namespace fsc { namespace db {
+
+namespace {
+
+struct SQLiteConnection : public Connection, kj::Refcounted {
+	// Interface methods
+	Own<Connection> addRef() override;
+	Own<Connection> fork(bool readOnly) override;
+	
+	Own<PreparedStatementHook> prepareHook(kj::StringPtr sql) override;
+	
+	// Implementation
+	SQLiteConnection(kj::StringPtr filename, bool readOnly);
+	~SQLiteConnection();
+	
+	int check(int result);
+	
+	// Members
+	kj::String filename;
+	sqlite3* handle;
+};
+
+struct SQLiteStatementHook : public PreparedStatementHook {
+	// Interface methods
+	void reset() override;
+	bool step() override;
+	
+	int64_t lastInsertedRowid() override;
+	size_t nRowsModified() override;
+	
+	void setParameter(size_t, double) override;
+	void setParameter(size_t, int64_t) override;
+	void setParameter(size_t, kj::ArrayPtr<const byte>) override;
+	void setParameter(size_t, kj::StringPtr) override;
+	void setParameter(size_t, decltype(nullptr)) override;
+	
+	double getDouble(size_t) override;
+	int64_t getInt64(size_t) override;
+	kj::ArrayPtr<const byte> getBlob(size_t) override;
+	kj::StringPtr getText(size_t) override;
+	
+	kj::String getColumnName(size_t) override;
+	
+	size_t size() override;
+	
+	// Implementation
+	SQLiteStatementHook(SQLiteConnection& parent, kj::StringPtr sql);
+	~SQLiteStatementHook();
+	
+	int check(int result) { return parent -> check(result); }
+	
+	// Members
+	Own<SQLiteConnection> parent;
+	sqlite3_stmt* handle;
+	bool available = false;
+};
+
+// class SQLiteConnection
+
+Own<Connection> SQLiteConnection::addRef() {
+	return kj::addRef(*this);
+}
+
+Own<Connection> SQLiteConnection::fork(bool readOnly) {
+	return kj::refcounted<SQLiteConnection>(filename, readOnly);
+}
+
+Own<PreparedStatementHook> prepareHook(kj::StringPtr sql) {
+	return SQLiteStatementHook(*this, sql);
+}
+
+SQLiteConnection(kj::StringPtr filename, bool readOnly) :
+	filename(kj::heapString(filename)),
+	handle(nullptr)
+{
+	try {
+		check(sqlite3_open(
+			filename.cStr(),
+			&handle,
+			(readOnly ? SQLITE_OPEN_READONLY : (SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE)) | SQLITE_OPEN_NOMUTEX | SQLITE_OPEN_EXRESCODE
+		));
+		
+		// Set up connection parameters
+		exec("PRAGMA journal_mode=WAL");
+		exec("PRAGMA foreign_keys = ON");
+	} catch(...) {
+		sqlite_close_v2(handle);
+		throw;
+	}
+}
+
+~SQLiteConnection() {
+	sqlite_close_v2(handle);
+}
+
+int SQLiteConnection::Check(int result) {
+	if(result == SQLITE_OK)
+		return result;
+	
+	if(result == SQLITE_ROW)
+		return result;
+	
+	if(result == SQLITE_DONE)
+		return result;
+	
+	if(result == SQLITE_BUSY) {
+		kj::throwFatalException(KJ_EXCEPTION(OVERLOADED, "Database busy"));
+	}
+	
+	int errorCode = sqlite3_errcode(handle);
+	int extendedErrorCode = sqlite3_extended_errcode(handle);
+	kj::String errorMessage = kj::str(sqlite3_errmsg(handle));
+	
+	KJ_FAIL_REQUIRE("SQL error in sqlite", errorCode, extendedErrorCode, errorMessage);
+}
+
+// class SQLiteStatementHook
+
+void SQLiteStatementHook::reset() {
+	available = false;
+	check(sqlite3_reset(handle));
+}
+
+bool SQLiteStatementHook::step() {
+	int result = check(sqlite3_step());
+	
+	available = true;
+	if(result == SQLITE_ROW)
+		return true;
+	
+	available = false;
+	if(result == SQLITE_DONE)
+		return false;
+	
+	KJ_UNREACHABLE();
+}
+
+int64_t SQLiteStatementHook::lastInsertedRowid() {
+	return sqlite3_last_insert_rowid(handle);
+}
+
+size_t SQLiteStatementHook::nRowsModified() {
+	return sqlite3_changes64(handle);
+}
+
+void SQLiteStatementHook::setParameter(size_t idx, double d) {
+	check(sqlite3_bind_double(handle, idx, d));
+}
+
+void SQLiteStatementHook::setParameter(size_t t, int64_t i) {
+	check(sqlite3_bind_int64(handle, idx, i));
+}
+
+void SQLiteStatementHook::setParameter(size_t idx, decltype(nullptr)) {
+	check(sqlite3_bind_null(handle, idx));
+}
+
+void SQLiteStatementHook::setParameter(size_t idx, ArrayPtr<const byte> blob) {
+	check(sqlite3_bind_blob(handle, idx, blob.begin(), blob.size(), SQLITE_TRANSIENT));
+}
+
+void SQLiteStatementHook::setParameter(size_t idx, kj::StringPtr text) {
+	check(sqlite3_bind_text(handle, idx, text.begin(), text.size(), SQLITE_TRANSIENT));
+}
+
+double SQLiteStatementHook::getDouble(size_t idx) {
+	KJ_REQUIRE(available, "Statement has no active row");
+	
+	return sqlite3_column_double(handle, idx);
+}
+
+int64_t SQLiteStatementHook::getInt64(size_t idx) {
+	KJ_REQUIRE(available, "Statement has no active row");
+	
+	return sqlite3_column_int64(handle, idx);
+}
+
+ArrayPtr<const byte> SQLiteStatementHook::getBlob(size_t idx) {
+	KJ_REQUIRE(available, "Statement has no active row");
+	
+	return kj::ArrayPtr<const byte>(
+		(const byte*) sqlite3_column_blob(handle, idx),
+		sqlite3_column_bytes(handle, idx)
+	);
+}
+
+kj::StringPtr SQLiteStatementHook::getText(size_t idx) {
+	KJ_REQUIRE(available, "Statement has no active row");
+	
+	return kj::StringPtr(
+		(const char*) sqlite3_column_text(handle, idx),
+		sqlite3_column_bytes(handle, idx)
+	);
+}
+
+kj::String SQLiteStatementHook::getColumnName(size_t idx) {
+	KJ_REQUIRE(available, "Statement has no active row");
+	
+	return kj::heapString(sqlite3_column_name(handle, idx));
+}
+
+size_t SQLiteStatementHook::size() {
+	KJ_REQUIRE(available, "Statement has no active row");
+	
+	return sqlite3_column_count(handle);
+}
+
+SQLiteStatementHook::SQLiteStatementHook(SQLiteConnection& parent, kj::StringPtr sql) :
+	parent(kj::addRef(parent))
+{
+	check(sqlite3_prepare_v2(
+		parent.handle,
+		sql.begin(), sql.size(),
+		&handle,
+		nullptr
+	));
+}
+
+SQLiteStatementHook::~SQLiteStatementHook() {
+	sqlite3_finalize(handle);
+}
+
+}
+
+}}
+
+namespace fsc {
+	
+Own<db::Transaction> connectSqlite(kj::StringPtr url, bool readOnly) {
+	return kj::refcounted<SQLiteConnection>(url, readOnly);
+}
+
+}
--- src/c++/fsc/db.h
+++ src/c++/fsc/db.h
@@ -2,270 +2,182 @@
 
 #include "common.h"
 
-#include <kj/refcount.h>
+namespace fsc { namespace db {
 
-#include <utility>
-
-// SQLite3 structs
-struct sqlite3;
-struct sqlite3_stmt;
-
-namespace fsc {
-
-struct SQLite3Connection;
-struct SQLite3PreparedStatement;
-struct SQLite3Blob;
-struct SQLite3Savepoint;
-struct SQLite3Transaction;
-struct SQLite3RootTransaction;
-
-enum class SQLite3Type;
-
-namespace sqlite {
-	using Connection = SQLite3Connection;
-	using Statement = SQLite3PreparedStatement;
-	using Blob = SQLite3Blob;
-	using Savepoint = SQLite3Savepoint;
-	using Transaction = SQLite3Transaction;
-	using RootTransaction = SQLite3RootTransaction;
-	using Type = SQLite3Type;
-}
-
-enum class SQLite3Type {
-	INTEGER = 1,
-	FLOAT = 2,
-	TEXT = 3,
-	BLOB = 4,
-	NULLTYPE = 5
+struct PreparedStatementHook {
+	virtual void reset() = 0;
+	virtual bool step() = 0;
+	
+	virtual int64_t lastInsertedRowid() = 0;
+	virtual size_t nRowsModified() = 0;
+	
+	virtual void setParameter(size_t, double) = 0;
+	virtual void setParameter(size_t, int64_t) = 0;
+	virtual void setParameter(size_t, kj::ArrayPtr<const byte>) = 0;
+	virtual void setParameter(size_t, kj::StringPtr) = 0;
+	virtual void setParameter(size_t, decltype(nullptr)) = 0;
+	
+	virtual double getDouble(size_t) = 0;
+	virtual int64_t getInt64(size_t) = 0;
+	virtual kj::ArrayPtr<const byte> getBlob(size_t) = 0;
+	virtual kj::StringPtr getText(size_t) = 0;
+	
+	virtual kj::String getColumnName(size_t) = 0;
+	virtual size_t size() = 0;
 };
 
-Own<SQLite3Connection> openSQLite3(kj::StringPtr filename);
+struct PreparedStatement {
+	PreparedStatement(Own<PreparedStatementHook>&& hook);
+	~PreparedStatement();
 	
-struct SQLite3Connection : private kj::Refcounted {
-	sqlite3* handle;
+	template<typename P>
+	setParameter(size_t, P);
 	
-	~SQLite3Connection();
+	//! Assigns all parameters from arguments
+	template<typename... Params>
+	void bind(Params... params);
 	
-	void check(int retCode);
-	Own<SQLite3Connection> addRef();
+	struct Column;
+	Column operator[](size_t idx);
 	
-	SQLite3PreparedStatement prepare(kj::StringPtr statement);
+	bool step();
 	
-	int64_t exec(kj::StringPtr statement);
-	int64_t execInsert(kj::StringPtr statement);
-	
-	int64_t lastInsertRowid();
-	int64_t nRowsModified();
-	
-	bool inTransaction();
-	
-	SQLite3Transaction beginTransaction(kj::StringPtr name = nullptr);
-	SQLite3RootTransaction beginRootTransaction(bool immediate);
-	Maybe<SQLite3RootTransaction> ensureTransaction(bool immediate);
-	
-private:
-	uint64_t transactionUID = 0; //< Unique ID counter for transactions
-	SQLite3Connection(kj::StringPtr filename);
-	
-	friend kj::Refcounted;
-	
-	template<typename T, typename... Params>
-	friend Own<T> kj::refcounted(Params&&... params);
-	
-	template <typename T>
-	friend Own<T> kj::addRef(T& object);
-};
-
-struct SQLite3PreparedStatement {
-	/** Column accessor helper.
-	 * 
-	 * Meant to be use like "int iVal = statement[0];" or "auto iVal = statement[0].asInt();".
-	 */
-	struct Column {
-		SQLite3PreparedStatement& parent;
-		int idx;
-		
-		operator kj::ArrayPtr<const byte>();
-		operator kj::StringPtr();
-		operator double();
-		operator int();
-		operator int64_t();
-		
-		SQLite3Type type();
-		
-		inline kj::ArrayPtr<const byte> asBlob() { return operator kj::ArrayPtr<const byte>(); }
-		inline double asDouble() { return operator double(); }
-		inline int asInt() { return operator int(); }
-		inline int64_t asInt64() { return operator int64_t(); }
-		inline kj::StringPtr asText() { return operator kj::StringPtr(); }
-		
-		kj::String name();
-		
-		inline bool operator==(const Column& other) { return other.idx == this->idx; }
-		inline bool operator!=(const Column& other) { return other.idx != this->idx; }
-		inline Column& operator++() { ++idx; return *this; }
-		
-		inline Column* operator ->() { return this; }
-		inline Column& operator  *() { return *this; }
-	};
-	
-	/** Parameter accessor helper.
-	 *
-	 * Meant to be used like "int i = 0; statement.param(0) = i;"
-	 */
-	struct Param {
-		SQLite3PreparedStatement& parent;
-		int idx;
-		
-		Param& operator=(kj::ArrayPtr<const byte> blob);
-		Param& operator=(kj::StringPtr text);
-		Param& operator=(int intVal);
-		Param& operator=(int64_t int64);
-		Param& operator=(double doubleVal);
-		Param& operator=(decltype(nullptr) nPtr);
-		
-		void setZero(size_t size);
-	};
-	
-	SQLite3PreparedStatement(SQLite3Connection& conn, kj::StringPtr statement);
-	
-	inline SQLite3PreparedStatement() : handle(nullptr) {}
-	~SQLite3PreparedStatement();
-	
-	SQLite3PreparedStatement(SQLite3PreparedStatement&&) = default;
-	SQLite3PreparedStatement& operator=(SQLite3PreparedStatement&&) = default;
-	
-	inline Param param(int i) { return Param {*this, i}; }
-	inline Column column(int i) { KJ_REQUIRE(state == ACTIVE); return Column {*this, i}; }
-	
-	inline Param operator[](int i) { return param(i); }
-	
-	struct Query {
-		SQLite3PreparedStatement& parent;
-		bool step();
-		
-		Query(SQLite3PreparedStatement& parent);
-		Query(Query&& other);
-		~Query();
-	
-		inline Column operator[](int i) { return parent.column(i); }
-		inline Column begin() { return parent.column(0); }
-		inline Column end() { return parent.column(parent.size()); }
-		
-	private:
-		kj::UnwindDetector ud;
-		bool movedFrom = false;
-	};
-	
-	void reset();
-	
-	int size();
-	
-	int64_t exec();
-	int64_t execInsert();
+	inline bool reset() { hook -> reset(); }
 	
 	template<typename... Params>
-	void bind(Params... params) {
-		bindInternal(std::index_sequence_for<Params...>(), params...);
-	}
-	
-	template<typename... Params>
-	int64_t operator()(Params... params) {
+	size_t operator()(Params... params) {
 		bind(params...);
-		return exec();
+		
+		hook -> step();
+		hook -> reset();
+		
+		return hook -> nRowsModified();
 	}
 	
 	template<typename... Params>
 	int64_t insert(Params... params) {
 		bind(params...);
-		return execInsert();
+		
+		hook -> step();
+		hook -> reset();
+		
+		return hook -> lastInsertedRowid();
 	}
 	
-	template<typename... Params>
-	Query query(Params... params) {
-		bind(params...);
-		return Query(*this);
+	inline bool step() {
+		if(hook -> step()) {
+			return true;
+		} else {
+			hook -> reset();
+			return false;
+		}
 	}
-	
-	sqlite3_stmt * handle;
-	Own<SQLite3Connection> parent;
 	
 private:
-	enum {
-		ACTIVE,
-		DONE,
-		READY
-	} state = READY;
-	
-	void check(int retCode);
-	
+	//! Helper method for binding with known index sequence
 	template<typename... Params, size_t... indices>
-	void bindInternal(std::integer_sequence<size_t, indices...> pIndices, Params... params) {
-		int unused[] = {
-			0, (this->param(indices + 1) = params, 1)...
-		};
-	}
+	void bindInternal(std::integer_sequence<size_t, indices...> pIndices, Params... params);
 	
-	bool step();
+	Own<PreparedStatementHook> hook;
 };
 
-/** Creates and maintains a savepoint. The savepoint is released without rollback on destruction.
- */
-struct SQLite3Savepoint {
-	SQLite3Savepoint(SQLite3Connection& conn, kj::StringPtr name);
-	SQLite3Savepoint(const SQLite3Savepoint&) = delete;
-	SQLite3Savepoint(SQLite3Savepoint&&) = default;
+struct PreparedStatement::Column {
+	PreparedStatement& parent;
+	size_t idx;
 	
-	~SQLite3Savepoint();
+	inline Column(PreparedStatement& parent, size_t idx) :
+		parent(parent), idx(idx)
+	{}
 	
-	void rollback();
+	inline kj::ArrayPtr<const byte> asBlob() { return parent.hook -> getBlob(idx); }
+	inline double asDouble() { return parent.hook -> getDouble(idx); }
+	inline int64_t asInt64() { return parent.hook -> getInt64(idx); }
+	inline kj::StringPtr asText() { return parent.hook -> getText(idx); }
+	
+	inline operator kj::ArrayPtr<const byte>() { return asBlob(); }
+	inline operator kj::StringPtr() { return asText(); }
+	inline operator double() { return asDouble(); }
+	inline operator int64_t() { return asInt64() ; }
+	
+	inline kj::String name() { return parent.hook -> getColumnName(idx); }
+	
+	inline bool operator==(const Column& other) { return other.idx == this->idx; }
+	inline bool operator!=(const Column& other) { return other.idx != this->idx; }
+	inline Column& operator++() { ++idx; return *this; }
+	
+	inline Column* operator ->() { return this; }
+	inline Column& operator  *() { return *this; }
+};
+
+struct Connection {
+	virtual Own<Connection> addRef() = 0;
+	virtual Own<Connection> fork(bool readOnly) = 0;
+	
+	virtual Own<PreparedStatementHook> prepareHook(kj::StringPtr sql) = 0;
+	
+	inline PreparedStatement prepare(kj::StringPtr sql) { return prepareHook(sql); }
+	inline int64_t exec(kj::StringPtr sql) { return prepare(sql)(); }
+
+private:
+	uint64_t savepointCounter;
+	friend class Savepoint;
+};
+
+//! A savepoint that can be acquired and later released (default behavior) or rolled back
+struct Savepoint {
+	Own<Connection> parent;
+	uint64_t id;
+	kj::UnwindDetector ud;
+	
+	Savepoint(Connection& parent);
+	~Savepoint();
+	
+	bool active();
 	void release();
-	
-	bool isReleased() { return released; }
-	
-private:
-	Own<SQLite3Connection> conn;
-	kj::String name;
-	bool released = false;
-	
-	kj::UnwindDetector ud;
-};
-
-/** Transaction class.
- *
- * Creates and maintains a savepoint that records all sql statements. Upon destruction, the savepoint
- * will be released if the object is destroyed normally, but will be rolled back upon an exception. 
- */
-struct SQLite3Transaction {
-	inline SQLite3Transaction(SQLite3Connection& conn, kj::StringPtr name) : savepoint(conn, name) {}
-	SQLite3Transaction(SQLite3Transaction&&) = default;
-	~SQLite3Transaction();
-	
-	inline void commit() { savepoint.release(); }
-	inline void rollback() { savepoint.rollback(); }
-	
-private:
-	SQLite3Savepoint savepoint;
-	kj::UnwindDetector ud;
-};
-
-/** Transaction class.
- *
- * Creates and maintains a savepoint that records all sql statements. Upon destruction, the savepoint
- * will be released if the object is destroyed normally, but will be rolled back upon an exception. 
- */
-struct SQLite3RootTransaction {
-	inline SQLite3RootTransaction(SQLite3Connection& conn, bool immediate);
-	SQLite3RootTransaction(SQLite3RootTransaction&&) = default;
-	~SQLite3RootTransaction();
-	
-	void commit();
 	void rollback();
-	
-private:
-	Own<SQLite3Connection> conn;
-	kj::UnwindDetector ud;
-	bool active = true;
 };
 
+//! A savepoint that will roll back on stack unwind
+struct Transaction {
+	Savepoint savepoint;
+	
+	Transaction(Connection& parent);
+	~Transaction();
+	
+	inline void commit() {savepoint.release(); }
+	inline void rollback() { savepoint.rollback(); }
+};
+
+}}
+
+namespace fsc {
+	Own<db::Connection> connectSqlite(kj::StringPtr url, bool readOnly = false);
 }
+
+// Implementation
+
+
+namespace fsc { namespace db {
+
+PreparedStatement::Column PreparedStatement::operator[](size_t idx) {
+	return Column(*this, idx);
+}
+
+template<typename P>
+void PreparedStatement::setParameter(size_t idx, P p) {
+	hook -> setParameter(idx, p);
+}
+
+template<typename... Params>
+void PreparedStatement::bind(Params... params) {
+	bindInternal(std::index_sequence_for<Params...>(), params...);
+}
+	
+template<typename... Params, size_t... indices>
+void bindInternal(std::integer_sequence<size_t, indices...> pIndices, Params... params) {
+	int unused[] = {
+		0, (hook -> setParameter(indices, params), 1)...
+	};
+}
+	
+}}
