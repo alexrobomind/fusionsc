2b9debf78136cc94e93eceb8bc4bce2d11a4c257
Alexander Knieps <a.knieps@fz-juelich.de>
2024-04-15 15:56:22 +0200

    Removed setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from skbuild import setup
-from setuptools import find_packages
-
-setup(
-	packages = ['fusionsc', 'fusionsc.devices', 'fusionsc.devices.w7x', 'fusionsc.devices.jtext'],
-	package_dir = {"" : "src/python"},
-	cmake_install_target = "fsc-install-skbuild",
-)
