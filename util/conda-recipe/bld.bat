e628b4cd177797898970607ff6e6ca53892d19ef
Alexander Knieps <s6alknie@uni-bonn.de>
2024-07-03 10:35:39 +0200

    Attempt to get shared lib build to work

--- util/conda-recipe/bld.bat
+++ util/conda-recipe/bld.bat
@@ -1,5 +1,5 @@
 mkdir build
 cd build
-cmake -DFSC_DEP_PREF_VENDORED=Off -DFSC_DEP_IGNORE_VERSIONS=Off -DBUILD_SHARED_LIBS=On ..
-cmake --build . --target fsc-python-bindings fsc-tool
-cmake --install .
+cmake -DFSC_DEP_PREF_VENDORED=Off -DFSC_DEP_IGNORE_VERSIONS=On -DBUILD_SHARED_LIBS=On ..
+cmake --build . --target fsc-python-bindings fsc-tool --config Release
+cmake --install . --config Release
