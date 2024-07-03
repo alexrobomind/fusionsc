c4aa47b70ad1eeb0f4b669e18a482f3b2f6a7ca8
Alexander Knieps <s6alknie@uni-bonn.de>
2024-07-03 18:39:06 +0200

    More fixes ...

--- src/c++/fsc/services.cpp
+++ src/c++/fsc/services.cpp
@@ -147,7 +147,7 @@ Promise<Temporary<Warehouse::StoredObject>> connectWarehouse(kj::StringPtr urlSt
 			Temporary<Warehouse::StoredObject> result;
 			result.setFolder(root);
 			result.setAsGeneric(root.castAs<Warehouse::GenericObject>());
-			return result;
+			return mv(result);
 		}
 	});
 }
