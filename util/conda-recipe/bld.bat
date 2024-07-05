731ff3200aedf0d76b5e936a0ff30b78d96be00e
Alexander Knieps <s6alknie@uni-bonn.de>
2024-07-05 10:46:14 +0200

    Install rule updates

--- util/conda-recipe/bld.bat
+++ util/conda-recipe/bld.bat
@@ -2,12 +2,12 @@ mkdir build
 cd build
 
 cmake -E echo === Configuring ===
-cmake -E echo Config command command cmake %CMAKE_ARGS% -DFSC_DEP_PREF_VENDORED=Off -DFSC_DEP_IGNORE_VERSIONS=On -DFSC_PYLIB_DIR:PATH=%SP_DIR% ..
-cmake %CMAKE_ARGS% -DFSC_DEP_PREF_VENDORED=Off -DFSC_DEP_IGNORE_VERSIONS=On -DFSC_PYLIB_DIR:PATH=%SP_DIR% ..
+cmake -E echo Config command command cmake %CMAKE_ARGS% -DFSC_DEP_PREF_VENDORED=Off -DFSC_DEP_IGNORE_VERSIONS=On -DFSC_PYLIB_DIR:PATH=%SP_DIR% -DFSC_WITH_PYTHON=On ..
+cmake %CMAKE_ARGS% -DFSC_DEP_PREF_VENDORED=Off -DFSC_DEP_IGNORE_VERSIONS=On -DFSC_PYLIB_DIR:PATH=%SP_DIR% -DPython_FIND_VIRTUALENV=ONLY ..
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 cmake -E echo === Building ===
-cmake --build . --config Release 2>error.txt
+cmake --build . --config Release 2>&1
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 cmake -E echo === Installing ===
