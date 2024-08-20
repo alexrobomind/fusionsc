5b85b23a37d545a2af3bf8bbf3548a5e704f604c
Michael Endler <mendler@mleim1.ipp-hgw.mpg.de>
2024-08-20 14:44:55 +0200

    Conda recipe progress on UNIX builds

---
+++ util/conda-recipe/build.sh
@@ -0,0 +1,12 @@
+mkdir build
+cd build
+
+cmake -E echo === Configuring ===
+cmake -E echo Config command command cmake ${CMAKE_ARGS} -DFSC_DEP_PREF_VENDORED=Off -DFSC_DEP_IGNORE_VERSIONS=On -DFSC_PYLIB_DIR:PATH=${SP_DIR} -DFSC_WITH_PYTHON=On ..
+cmake --debug-find ${CMAKE_ARGS} -DFSC_DEP_PREF_VENDORED=Off -DFSC_DEP_IGNORE_VERSIONS=On -DFSC_PYLIB_DIR:PATH=${SP_DIR} ..
+
+cmake -E echo === Building ===
+cmake --build . --config Release 2>&1
+
+cmake -E echo === Installing ===
+cmake --install . --config Release 2>&1
