0d4d1c3d1587d7bf65f9691e79e08bc1a96c4c3f
Alexander Knieps <a.knieps@fz-juelich.de>
2024-09-05 04:35:05 +0200

    Added ninja build

--- util/conda-recipe/bld.bat
+++ util/conda-recipe/bld.bat
@@ -2,8 +2,8 @@ mkdir build
 cd build
 
 cmake -E echo === Configuring ===
-cmake -E echo Config command command cmake %CMAKE_ARGS% -DFSC_DEP_PREF_VENDORED=Off -DFSC_DEP_IGNORE_VERSIONS=On -DFSC_PYLIB_DIR:PATH=%SP_DIR% -DFSC_WITH_PYTHON=On ..
-cmake %CMAKE_ARGS% -DFSC_DEP_PREF_VENDORED=Off -DFSC_DEP_IGNORE_VERSIONS=On -DFSC_PYLIB_DIR:PATH=%SP_DIR% -DPython_FIND_VIRTUALENV=ONLY ..
+cmake -E echo Config command command cmake -G Ninja %CMAKE_ARGS% -DFSC_DEP_PREF_VENDORED=Off -DFSC_DEP_IGNORE_VERSIONS=On -DFSC_PYLIB_DIR:PATH=%SP_DIR% -DFSC_WITH_PYTHON=On ..
+cmake -G Ninja %CMAKE_ARGS% -DFSC_DEP_PREF_VENDORED=Off -DFSC_DEP_IGNORE_VERSIONS=On -DFSC_PYLIB_DIR:PATH=%SP_DIR% -DPython_FIND_VIRTUALENV=ONLY ..
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 cmake -E echo === Building ===
